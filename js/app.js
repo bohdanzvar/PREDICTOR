8420db387c63e8ec4107cedd686ad5643c0d9e61
bohdanzvar <113348286+bohdanzvar@users.noreply.github.com>
2023-06-29 18:11:12 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@
     }), 0);
     let x;
     document.getElementById("rollButton").onclick = function() {
-        x = Math.random() * 6 + 2;
+        x = Math.random() * 6 ;
         x = (Math.round(x * 100) / 100).toFixed(2);
         document.getElementById("xlabel").innerHTML = x;
     };
